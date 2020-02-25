eb1edec7fe5b4194781daedaa3bf3c437fd340bb
ivanlhz <ivanlhz@gmail.com>
2020-02-25 10:19:33 +0000

    refactor babel config

--- .babelrc.js
+++ .babelrc.js
@@ -1,15 +1,16 @@
 module.exports ={
   presets: [
     [
-      "@babel/env",
+      '@babel/env',
       {
-        "targets": {
-          "edge": "17",
-          "firefox": "60",
-          "chrome": "67",
-          "safari": "11.1",
+        'corejs': '3',
+        'targets': {
+          'browsers': [
+            'last 5 versions',
+            'ie >= 8'
+          ]
         },
-        "useBuiltIns": "usage",
+        'useBuiltIns': 'usage',
       }
     ],
     '@babel/preset-react'
