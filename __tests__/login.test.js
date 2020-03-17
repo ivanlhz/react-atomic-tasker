11ee6d2b34dae525d8103c7a6f6f87f8aa063c23
ivanlhz <ivanlhz@gmail.com>
2020-03-17 06:22:17 +0000

    Test login fails

--- __tests__/login.test.js
+++ __tests__/login.test.js
@@ -1,14 +1,21 @@
 import React from 'react'
-import {render} from '@testing-library/react'
+import {render, wait} from '@testing-library/react'
 import {MemoryRouter} from 'react-router-dom'
 import {AuthContext} from '../src/hooks/auth'
 import userEvent from '@testing-library/user-event'
 import {Login} from '../src/components'
 import FireApp from '../src/api/firebase/'
+// import {Redirect as MockRedirect} from 'react-router'
 
 FireApp.auth = jest.fn().mockReturnValue({
   signInWithEmailAndPassword: jest.fn(() => Promise.resolve(null)),
 })
+
+// jest.mock('react-router', () => {
+//   return {
+//     Redirect: jest.fn(() => null),
+//   }
+// })
 
 test('Should render the login', () => {
   const {getByTestId} = render(
@@ -26,6 +33,9 @@ test('Should  call signInWithEmailAndPassword', () => {
     name: 'ivanlhz@gmail.com',
     pwd: '1234',
   }
+  FireApp.auth = jest.fn().mockReturnValue({
+    signInWithEmailAndPassword: jest.fn(() => Promise.resolve(null)),
+  })
 
   const {getByText, getByLabelText} = render(
     <MemoryRouter initialEntries={['/']}>
@@ -41,4 +51,34 @@ test('Should  call signInWithEmailAndPassword', () => {
   userEvent.type(inputPwd, user.pwd)
   userEvent.click(submitBtn)
   expect(FireApp.auth().signInWithEmailAndPassword).toHaveBeenCalledTimes(1)
+})
+
+test('Should reject the call signInWithEmailAndPassword and show the error message', () => {
+  const user = {
+    name: 'ivanlhz@gmail.com',
+    pwd: '1234',
+  }
+  FireApp.auth = jest.fn().mockReturnValue({
+    signInWithEmailAndPassword: jest.fn(() =>
+      Promise.reject({message: 'Login fail'}),
+    ),
+  })
+
+  const {getByText, getByLabelText} = render(
+    <MemoryRouter initialEntries={['/']}>
+      <AuthContext.Provider value={{currentUser: undefined}}>
+        <Login />
+      </AuthContext.Provider>
+    </MemoryRouter>,
+  )
+  const submitBtn = getByText(/Log in/i)
+  const inputEmail = getByLabelText(/email/i)
+  const inputPwd = getByLabelText(/password/i)
+  userEvent.type(inputEmail, user.name)
+  userEvent.type(inputPwd, user.pwd)
+  userEvent.click(submitBtn)
+  expect(FireApp.auth().signInWithEmailAndPassword).toHaveBeenCalledTimes(1)
+  wait(() => {
+    expect(getByText(/Login fail/i)).toBeTruthy()
+  })
 })
